7e4d21e3087471fdfd903495d0a59e29767d0724
DimedroLich <1DimedroL1@gmail.com>
2023-04-04 12:36:07 +0300

    Added all products and products categories to db via admin panel. Refactored templates for 'products.html' with getting info exactly from db without hardcoding. Images currently unavailable. 3.7 - DONE

--- products/models.py
+++ products/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
     short_description = models.CharField(max_length=64,blank=True)
-    price = models.DecimalField(max_digits=6,decimal_places=2)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory,on_delete=models.CASCADE)
