fad7be5b60462d490b298bd1e117d57df313ad4f
dimedrol <1dimedrol1@gmail.com>
2023-05-24 11:05:36 +0300

    Added Users app and model. 4.6 - DONE

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Product,ProductCategory
+# Register your models here.
+
 
 admin.site.register(Product)
 admin.site.register(ProductCategory)
