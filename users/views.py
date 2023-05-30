54ce586ba727a0b620aeb9e5e0aca2a89d91b894
dimedrol <1dimedrol1@gmail.com>
2023-05-30 20:06:56 +0300

    - Added autentification forms file to users app. - Auth form displays on login page. 4.8 - DONE

--- users/views.py
+++ users/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from .forms import UserLoginForm
+
 
 # Create your views here.
 
 def login(request):
-    return render(request, 'users/login.html')
+    form = UserLoginForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'users/login.html', context=context)
+
 
 def registration(request):
     return render(request, 'users/registration.html')
