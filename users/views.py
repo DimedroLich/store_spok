fb3a571ede8703da960e7cf84b65c6d0adefe7df
dimedrol <1dimedrol1@gmail.com>
2023-06-03 12:13:28 +0300

    - Added logic to process POST requests to 'users:login' view.

--- users/views.py
+++ users/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import UserLoginForm
+from django.contrib import auth
 
 
 # Create your views here.
 
 def login(request):
-    form = UserLoginForm()
+    """Обрабатывает страницу логина"""
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():  # Обязательная валидация при обработке POST
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username,
+                                     password=password)  # Аутентификация пользователся с помощью переданных через POST данных
+            if user:
+                auth.login(request, user)
+                return redirect('index')
+
+    form = UserLoginForm()  # Логика, если метод = GET
     context = {
         'form': form
     }
