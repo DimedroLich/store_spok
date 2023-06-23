51c4a3852652e667493f0f7b41940f01d3f759b1
dimedrol <1dimedrol1@gmail.com>
2023-06-23 11:38:49 +0300

    - Added possibility to show products by there category on main products page. 6.2 - DONE

--- products/urls.py
+++ products/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.ProductsViev.as_view(),name='index'),
+    path('', views.products, name='index'),
+    path('<int:category_id>/', views.products, name='category'),
     path('basket/add/<int:product_id>', views.basket_add, name='basket_add'),
     path('basket/remove/<int:basket_id>', views.basket_remove, name='basket_remove'),
-    path('<int:product_id>/',views.about_product,name='about'),
+    path('<int:product_id>/', views.about_product, name='about'),
 
 ]
-
--- products/views.py
+++ products/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.views.generic import ListView, DetailView
 
 from .models import Product, ProductCategory, Basket
 from users.models import User
-from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
@@ -16,48 +17,61 @@ def index(request):
     return render(request, 'products/index.html', context=context)
 
 
-class ProductsViev(ListView):
-    """Отображение всех товаров из б/д"""
-    template_name = 'products/products.html'
-    model = Product
-    context_object_name = 'products'
-    extra_context = {'title': 'Товары в магазине Бибасика Бобова', }
+# class ProductsViev(ListView):
+#     """
+#     Отображение всех товаров из б/д
+#     CBV работает корректно, но для отображения фильтрованных товаров по категориям, пока вернёмся к функции
+#     """
+#     template_name = 'products/products.html'
+#     model = Product
+#     context_object_name = 'products'
+#     extra_context = {'title': 'Товары в магазине Бибасика Бобова', }
+#
+#     def get_context_data(self, *, object_list=None, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['categories'] = ProductCategory.objects.all()
+#         return context
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['categories'] = ProductCategory.objects.all()
-        return context
+
+def products(request, category_id=None,
+             page=1):  # Если категория не передаётся, то дефолтное значение = None, иначе функция будет требовать аргумент. Аргумент page - нужен для пагинатора
+    """
+    Получение списка либо всех продуктов в магазине, либо продуктов по выбранной категории
+    """
+    products = Product.objects.filter(
+        category_id=category_id) if category_id else Product.objects.all()  # Если категория передана, фильтруем товары по ней. Если нет, берём все товары
+    paginator = Paginator(object_list=products, per_page=2) # Добавление пагинатора для постраничного вывода товаров
+    # products_paginator =
+    context = {
+        'title': 'Товары в магазине Бибасика Бобова',
+        'products': products,
+        'categories': ProductCategory.objects.all()
+    }
+    return render(request, 'products/products.html', context=context)
 
 
-# def products(request):
-#     context = {
-#         'title': 'Товары в магазине Бибасика Бобова',
-#         'products' : Product.objects.all(),
-#         'categories' : ProductCategory.objects.all()
-#     }
-#     return render(request,'products/products.html',context=context)
-
-@login_required   # Проверка залогинен ли пользователь если нет, происходит редирект на страницу логина. Страница редиректа указана в settings
+@login_required  # Проверка залогинен ли пользователь если нет, происходит редирект на страницу логина. Страница редиректа указана в settings
 def basket_add(request, product_id):
     """Контроллер добавления продукта в корзину товаров. Называется 'обработчик событий'"""
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
 
-    if not baskets.exists(): # Если в корзине нет никаких товаров
+    if not baskets.exists():  # Если в корзине нет никаких товаров
         Basket.objects.create(user=request.user, product=product, quantity=1)
-    else:   # Если есть, увеличиваем количество товара на 1
+    else:  # Если есть, увеличиваем количество товара на 1
         basket = baskets.first()
         basket.quantity += 1
         basket.save()
 
-    return redirect(request.META['HTTP_REFERER']) # Возвращение на ту страницу, где пользователь выполнял действие
+    return redirect(request.META['HTTP_REFERER'])  # Возвращение на ту страницу, где пользователь выполнял действие
+
 
 @login_required
 def basket_remove(request, basket_id):
     """Контроллер обработчик события. Удаление товара из корзины"""
     basket = Basket.objects.get(id=basket_id)
     basket.delete()
-    return redirect(request.META['HTTP_REFERER']) # Возвращение на ту страницу, где пользователь выполнял действие
+    return redirect(request.META['HTTP_REFERER'])  # Возвращение на ту страницу, где пользователь выполнял действие
 
 
 def about_product(request, product_id):
